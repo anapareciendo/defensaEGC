734bd50241c8047fc4406fbc94c8c83595d1b333
Ana Aparicio <ana.apariciora@gmail.com>
2018-01-17 18:54:15 +0100

    Test para busca votaciones

--- AdminVotaciones-EGC-G1-develop/principal/tests.py
+++ AdminVotaciones-EGC-G1-develop/principal/tests.py
@@ -7,6 +7,7 @@ Replace this with more appropriate tests for your application.
 
 from django.test import TestCase
 from principal.models import *
+from principal.views import busca_votaciones
 
 class SimpleTest(TestCase):
     def test_basic_addition(self):
@@ -15,12 +16,22 @@ class SimpleTest(TestCase):
         """
         self.assertEqual(1 + 1, 2)
         
-    def test_poll(self):
-        """
-        Test that the Poll is correctly created and saved in DB
-        """
-        ca = Ca.objects.create(id=1312, name="Josemita")
-        census = Census.objects.create(id=1222, title="Jos", postalCode=11510, ca=ca)
-        poll = Poll.objects.create(id=1312, title="Prueba", description="Votacion de prueba", startDate="2017-01-13", endDate="2018-01-10",census=census, participantes=0, votos=0)
-        self.assertEqual(poll.id, 1312)
+#    def test_poll(self):
+#        """
+#        Test that the Poll is correctly created and saved in DB
+#        """
+#        ca = Ca.objects.create(id=1312, name="Josemita")
+#        census = Census.objects.create(id=1222, title="Jos", postalCode=11510, ca=ca)
+#        poll = Poll.objects.create(id=1312, title="Prueba", description="Votacion de prueba", startDate="2017-01-13", endDate="2018-01-10",census=census, participantes=0, votos=0)
+#        self.assertEqual(poll.id, 1312)
+        
+    def test_busca_votaciones(self):
+       
+        bv = busca_votaciones()
+        
+        res = 1
+        if(len(bv) == 0):
+            res=0
+            
+        self.assertEqual(res,1)
         
